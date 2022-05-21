1f1894c5b5aeb239ab2f4ebf70c422be9506bb36
nicov2000 <nicolasvarg00@gmail.com>
2022-05-21 19:22:06 -0300

    web v0.9

---
+++ readme.md
@@ -0,0 +1,37 @@
+# Web Restaurante - Proyecto Codo a Codo 4.0
+
+## Propósito de la web
+
+Esta web esta principalmente orientada al uso por parte de restaurantes u otras industrias del rubro gastronómico que puedan ofrecer un delivery y que gran parte de su negocio sea a través de este servicio. Es decir, además de darle exposición al local, **la web se enfoca en conseguir que el cliente ordene de manera online**.
+
+---
+
+## Logica para conseguir el objetivo
+
+Como el objetivo es conseguir el pedido del cliente, existen varios botones Call to Action (contacto de whatsapp) para que el cliente decida ordenar, con un mensaje precargado.
+
+Si el cliente no hace click en el boton de la primer seccion pero sigue bastante interesado como para explorar la pagina, va a seguir leyendo, y los contenidos presentes van a cumplir el rol de generar momentum para que el cliente "tenga más ganas" de ordenar. Al llegar a la ultima sección de la pagina, se va a encontrar con el newsletter, donde podrá recibir un descuento + Free Delivery si decide suscribirse. De esta forma podremos obtener su email para el posterior email marketing.
+
+## Diseño
+
+Para la estructura de la web se eligió el modelo **One Page**, con 8 secciones. Es un template **mobile-first** y **responsive** creado desde 0, sin usar frameworks de CSS.
+
+---
+
+## Tecnologías utilizadas
+
+- HTML5
+- CSS3
+- Javascript
+
+---
+
+## Enlace
+
+Click en el enlace para ver el sitio: [name](link "Web Restaurante")
+
+---
+
+## Autor
+
+_- Nicolas Vargas (Comision Python 22078, prof Juan Cardone)_
---
+++ src/main.js
@@ -0,0 +1,127 @@
+const hamburgerBtn = document.querySelector(".hamburger");
+const navbarItems = document.querySelector(".navbar__ul");
+
+// Mobile Menu Toggle
+document.addEventListener("click", (click) => {
+	if(click.target === hamburgerBtn || clickedOutsideMenu(click)) {
+			toggleMobileMenu();
+	}
+});
+
+const clickedOutsideMenu = (click) => 
+menuIsActive() && click.target!== hamburgerBtn;
+
+const menuIsActive = () => 
+hamburgerBtn.classList.contains("active") && 
+!navbarItems.classList.contains("hidden");
+
+function toggleMobileMenu(){
+	navbarItems.classList.toggle("hidden"); 
+	hamburgerBtn.classList.toggle("active");
+}
+
+// About Us Section Toggle
+const aboutUsSection = document.querySelector("#about-us");
+const aboutUsOpenBtn = document.querySelector(".about-us-show-btn");
+const aboutUsCloseBtns = Array.from(document.querySelectorAll(".about-us-close-btn"));
+
+aboutUsOpenBtn.onclick = () => aboutUsSection.classList.toggle("hidden", false);
+
+aboutUsCloseBtns.forEach(button => 
+	button.onclick = () => {aboutUsSection.classList.toggle("hidden", true)}
+);
+
+// Menu Selector
+const categoryFilterBtn = document.querySelector("#menu__filter-btn");
+
+categoryFilterBtn.onclick = click => {
+  click.preventDefault();
+
+  const menuSelector = document.querySelector("#categories");
+  const selectedCategory = menuSelector.value.toLowerCase();
+  const allHTMLCategories = document.querySelectorAll(".food-category");
+
+  allHTMLCategories.forEach(category => {
+    category.classList.toggle("hidden", true);
+
+    if (category.id === selectedCategory) {
+      setTimeout(()=> category.classList.toggle("hidden", false), 500);
+    };
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Form Validation
+const form = document.querySelector("#form");
+
+form.onsubmit = function(event) {
+  event.preventDefault();
+  validateForm();
+};
+
+function validateForm() {
+  const firstname = document.querySelector("[name='firstname']");
+  const surname = document.querySelector("[name='surname']");
+  const email = document.querySelector("[name='email']");
+  const favouriteFood = document.querySelector("[name='favourite_food']");
+  const tycCheckbox = document.querySelector("[name='tyc']");
+  const emailMarketing = document.querySelector("[name='email_marketing']");
+
+  // if(true) { // testeando validaciones
+    console.log("%cpreventDefault() working! Now you can validate all fields and decide to submit or not.", "background-color: #292; padding: 10px; border-radius: 10px;");
+    console.log(`${firstname.name}: ${firstname.value}`);
+    console.log(`${surname.name}: ${surname.value}`);
+    console.log(`${email.name}: ${email.value}`);
+    console.log(`${favouriteFood.name}: ${favouriteFood.value}`);
+    console.log(`${tycCheckbox.name}: ${tycCheckbox.value}`);
+    console.log(`${emailMarketing.name}: ${emailMarketing.value}`);
+  //   return false;
+
+  setTimeout(function() {
+    
+    // if everything is ok...
+    form.submit()
+
+    // else abort submit and tell the client to check the form.
+    // code
+    // code
+  },8000)
+
+  // Formulario funcionando correctamente. Al enviar los datos se previene la conducta normal (enviar el formulario) y primero pasa por una funcion de validacion, para despues enviar (0 no) los datos, segun los criterios que deben cumplirse.
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
